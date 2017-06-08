1b02431e5e19f9872e00c796433ca5f5830fee2b
订单组-谭智军 <thanos_t@163.com>
2017-06-08 21:33:27 +0800

    启动脚本修改

---
+++ oauth-server/tools.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+case "$1" in
+ start)
+   java -Xms256m -Xmx512m -Dspring.profiles.active="$2" -jar ./oauth-server.jar >/dev/null 2>&1 &
+    exit 1
+ ;;
+ stop)
+  ps -ef|grep oauth-server.jar|awk '{print $2}'|while read pid
+  do
+    kill -15 $pid
+  done
+ ;;
+ *)
+  printf 'Usage: %s {start|stop}\n' "$prog"
+  exit 1
+ ;;
+esac
