cfdc6fa39681cd1c412068a5fcaaeabe7d0d99dd
订单组-谭智军 <thanos_t@163.com>
2017-06-08 21:22:52 +0800

    启动脚本修改

--- eureka-server/tools.sh
+++ eureka-server/tools.sh
@@ -2,18 +2,8 @@
 
 case "$1" in
  start)
-  case "$2" in
-   test | demo)
-    java -Xms256m -Xmx512m -Dspring.profiles.active="$2" -jar ./eureka-server.jar >/dev/null 2>&1 &
-   ;;
-   prod)
-    java -Xms256m -Xmx512m -Dspring.profiles.active="$2" -jar ./eureka-server.jar >/dev/null 2>&1 &
-   ;;
-   *)
-    printf 'append active profile: %s {test|demo|prod}\n' "$prog"
+   java -Xms256m -Xmx512m -Dspring.profiles.active="$2" -jar ./eureka-server.jar >/dev/null 2>&1 &
     exit 1
-   ;;
-  esac
  ;;
  stop)
   ps -ef|grep eureka-server.jar|awk '{print $2}'|while read pid
